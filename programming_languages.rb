e3ae85c95a4bb792c9e9a370e100067d1fdb96cf
w.h.sinn <w.h.sinn@gmail.com>
2019-03-26 19:09:12 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,6 +2,7 @@ def reformat_languages(languages)
   languages.each do |oo_or_functional, language_hash|
       language_hash.each do |language, attribute_hash|
         attribute_hash.each do |attribute, str_value|
+          if language_attributes[language].nil?
 
 end
 
